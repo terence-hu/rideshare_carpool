b2ba6f8e1f218eff2879ed5acc992d0ad9505e5f
Raghu Betina <raghu@firstdraft.com>
2019-12-07 22:20:00 +0000

    Only preferred_trip user can modify preferred_trip

--- app/controllers/preferred_trips_controller.rb
+++ app/controllers/preferred_trips_controller.rb
@@ -1,4 +1,14 @@
 class PreferredTripsController < ApplicationController
+  before_action :current_user_must_be_preferred_trip_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_preferred_trip_user
+    preferred_trip = PreferredTrip.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == preferred_trip.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @preferred_trips = PreferredTrip.all
 
